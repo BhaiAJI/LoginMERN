3e8944449b8e855e8e2080dbc6fa550bfe8acb1c
sirGaurav <gaurav@aresourcepool.com>
2023-05-10 12:16:51 +0530

    Checking for login

--- client/src/components/Password.js
+++ client/src/components/Password.js
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function Password() {
   return (
-    <div>Password</div>
+    <div>Gaurav Password</div>
   )
 }
--- client/src/components/Username.js
+++ client/src/components/Username.js
@@ -5,11 +5,25 @@ export default function Username() {
     <div className="container mx-auto">
       <div className='flex justify-center items-center h-screen'>
         <div>
-          <div className='title'>
-<h4>Hello Gaurav MERN</h4>
+          <div className='title flex flex-col items-center'>
+            <h4 className='text 5xl font-bold'>Hello Gaurav MERN</h4>
+            <span className='py-4 text-xl w-2/3 text-center text-gray-5'>
+              Explore More by connecting with us!
+            </span>
           </div>
+
+          <form className='py-1'>
+            <div className='profile flex justify-center py-4'>
+              <img src='https://fastly.picsum.photos/id/648/536/354.jpg?hmac=aiTRoFj_53OJi6_yAxgwFVIx99cVD05qpLBCWWj4zzg' alt='avtar' />
+            </div>
+
+            <div className='textbox'>
+              <input type="textinput" placeholder='Username' />
+              <button type='submit'>Let's go</button>
+            </div>
+          </form>
         </div>
       </div>
-      </div>
+    </div>
   )
 }
